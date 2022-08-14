c30155688e327b0d70e21cba730afe0a8df62e32
cdutten <cdutten@medallia.com>
2022-08-14 12:50:29 +0200

    wip

--- Dockerfile
+++ Dockerfile
@@ -22,6 +22,6 @@ COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 
-CMD [ "/bin/bash", "-c", "--", "while true; do python main.py; done; "]
+CMD ["python", "main.py"]
 
 
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -7,31 +9,31 @@ from pyvirtualdisplay import Display
 
 class Main:
     def __init__(self, url):
-        print("Ahoj!")
-
+        print("Starting...")
         # set xvfb display since there is no GUI in docker container.
-        display = Display(visible=0, size=(800, 600))
-        display.start()
-
+        self.display = Display(visible=0, size=(800, 600))
+        self.display.start()
         chrome_options = Options()
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument('--headless')
-        driver = webdriver.Chrome(options=chrome_options)
-        driver.get(url)
-        assert "Python" in driver.title
-        elem = driver.find_element(By.NAME, "q")
-        elem.clear()
-        elem.send_keys("pycon")
-        elem.send_keys(Keys.RETURN)
-        assert "No results found." not in driver.page_source
-        driver.close()
-        display.stop()
-        print("BIANCULI")
+        self.driver = webdriver.Chrome(options=chrome_options)
+
+        for i in range(2):
+            print(f"Running. Attempt: {i}")
+            self.check_title(url=url)
+            time.sleep(30)
+        self.driver.close()
+        self.display.stop()
+        print("Finished")
+
+    def check_title(self, url):
+        self.driver.get(url)
+        print(self.driver.title)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    Main("http://www.python.org")
+    Main("https://icp.administracionelectronica.gob.es/icpplus/index.html")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
